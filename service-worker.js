b9c5e7e79f61db9e0e92754dd5e012a6b97d7afe
Yeshua3x <rochaeterna12@outlook.com>
2026-02-12 04:38:23 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "pfconcursos-v1";
+const urlsToCache = ["index.html", "style.css", "manifest.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+
+
